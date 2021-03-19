3a7d7d58f04067edbdf1935b39ebd4ed9b87f759
Pablo Palomo <ppalomov@gmail.com>
2021-03-19 18:45:59 +0100

    Celebrity contract and tests created

--- contracts/CelebrityContract.sol
+++ contracts/CelebrityContract.sol
@@ -8,11 +8,66 @@ import "hardhat/console.sol";
  */
 contract CelebrityContract {
 
+    struct Celebrity {
+        string name;
+        uint price;
+        uint responseTime;
+    }
+
+    mapping(address => Celebrity) public celebrities;
+
+    event CelebrityCreated(address indexed owner, string name, uint price, uint responseTime);
+    event CelebrityDeleted(address indexed owner);
+    event CelebrityUpdated(address indexed owner, string name, uint price, uint responseTime);
+
     /**
      * Constructor method.
      */
     constructor () {}
 
-    
+    /**
+     * Function used to request a new NFT (autograph) to a celeb.
+     * - name: Celebrity name.
+     * - price: Celebrity current autograph price.
+     * - responseTime: Celebrity response time.
+     */
+    function createCelebrity(string memory name, uint price, uint responseTime) public {
+        Celebrity memory newCelebrity = Celebrity(name, price, responseTime);
+        celebrities[msg.sender] = newCelebrity;
+
+        emit CelebrityCreated(msg.sender, newCelebrity.name, newCelebrity.price, newCelebrity.responseTime);
+    }
+
+    /**
+     * Delete a celebrity.
+     */
+    function deleteCelebrity() public {
+        delete celebrities[msg.sender];
+        
+        emit CelebrityDeleted(msg.sender);
+    }
+
+    /**
+     * Update celebrity information.
+     * - name: Celebrity name.
+     * - price: Celebrity current autograph price.
+     * - responseTime: Celebrity response time.
+     */
+    function updateCelebrity(string memory name, uint price, uint responseTime) public {
+        celebrities[msg.sender].name = name;
+        celebrities[msg.sender].price = price;
+        celebrities[msg.sender].responseTime = responseTime;
+
+        emit CelebrityUpdated(msg.sender, name, price, responseTime);
+    }
+
+    /**
+     * Gets celebrity information by address.
+     * - addr: Celebrity account address.
+     */
+    function getCelebrity(address addr) public view returns (
+        string memory name, uint price, uint responseTime) {
+        return (celebrities[addr].name, celebrities[addr].price, celebrities[addr].responseTime);
+    }
 
 }
--- test/Tests.js
+++ test/Tests.js
@@ -8,6 +8,58 @@ use(solidity);
 
 describe("Hashink Contracts", function () {
 
+    let AutographRequestContract, AutographContract, CelebrityContract;
+    let requestsContract, autographContract, celebrityContract;
+    let owner;
+    let addr1;
+    let addr2;
+    let addrs;
+
+    beforeEach(async function () {
     
+        // Deploying celebrities contract
+        CelebrityContract = await ethers.getContractFactory("CelebrityContract");
+        celebrityContract = await CelebrityContract.deploy();
+        expect(celebrityContract.address).to.properAddress;
+
+        // Getting tests accounts
+        [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+
+    });
+
+    describe("Celebrity Contract", function() {
+
+        it("Should create a new celebrity", async function () { 
+            const name = "Justin Shenkarow";
+            const price = ethers.utils.parseEther('2');
+            const responseTime = 2;
+
+            await expect(
+                celebrityContract.connect(addr1).createCelebrity(name, price, responseTime)
+            )
+            .to.emit(celebrityContract, 'CelebrityCreated');
+            
+            const celeb = await celebrityContract.getCelebrity(addr1.address);
+            expect(celeb[0]).to.equal(name);
+            expect(celeb[1]).to.equal(price);
+            expect(celeb[2]).to.equal(responseTime);
+        });
+
+        it("Should update celebrity information", async function () { 
+            const name = "Justin Shenkarow";
+            const price = ethers.utils.parseEther('2');
+            const responseTime = 2;
+
+            await celebrityContract.connect(addr1).createCelebrity(
+                "Vitalik Buterin", ethers.utils.parseEther('1'), 4);                
+            await celebrityContract.connect(addr1).createCelebrity(name, price, responseTime);
+            
+            const celeb = await celebrityContract.getCelebrity(addr1.address);
+            expect(celeb[0]).to.equal(name);
+            expect(celeb[1]).to.equal(price);
+            expect(celeb[2]).to.equal(responseTime);
+        });
+    
+    });
 
 });
